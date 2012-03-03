d2347fab47748233706581205bc76d6508732a5c
Israel Levin <root@ars.(none)>
2012-03-03 23:10:46 +0200

    Default to duck

--- config/globals.lua
+++ config/globals.lua
@@ -60,7 +60,7 @@ search_engines = {
 }
 
 -- Set google as fallback search engine
-search_engines.default = search_engines.l
+search_engines.default = search_engines.sl
 -- Use this instead to disable auto-searching
 --search_engines.default = "%s"
 
